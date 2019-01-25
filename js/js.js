d478d85fe6d8506db62fd06be4b34b49977c6c78
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-01-25 15:39:39 -0200

    Adicionando login com o Google e com o Github...

--- js/js.js
+++ js/js.js
@@ -1,10 +1,28 @@
 const google = document.getElementById('google');
 const github = document.getElementById('github');
 
-google.onclick = function() {
+google.onclick = function () {
+    firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(function (user) {
 
+    }).catch(function (error) {
+        console.log(error);
+    });
 }
 
-google.onclick = function() {
-    
+github.onclick = function () {
+    firebase.auth().signInWithPopup(new firebase.auth.GithubAuthProvider()).then(function (user) {
+        
+    }).catch(function (error) {
+        consolel.log(error);
+    });
 }
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        console.log('Logado!');
+        console.log(user);
+    } else {
+        console.log('Deslogado!');
+    }
+    loading.style.display = 'none';
+});
